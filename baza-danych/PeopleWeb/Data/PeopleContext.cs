0a2dee3957760892be6f5447c1a807c2786b3a6e
sebastian-kowal <64037675+sebastian-kowal@users.noreply.github.com>
2021-08-15 18:27:21 +0200

    Add files via upload

---
+++ baza-danych/PeopleWeb/Data/PeopleContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections;
+
+namespace PeopleWeb.Data
+{
+    public class PeopleContext : DbContext
+    {
+        public PeopleContext(DbContextOptions<PeopleContext> options) 
+            : base(options)
+        {
+            Database.EnsureCreated();
+        }
+
+        public DbSet<Person> People { get; set; }
+    }
+}
---
+++ baza-danych/PeopleWeb/Data/Person.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PeopleWeb.Data
+{
+    public class Person
+    {        
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public string Id { get; set; }
+
+        [DisplayName("First name")]
+        public string FirstName { get; set; }
+
+        [DisplayName("Last name")]
+        public string LastName { get; set; }
+
+        [DisplayName("Birth date")]
+        public DateTime BirthDate { get; set; }        
+    }
+}
